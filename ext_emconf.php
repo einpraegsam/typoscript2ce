6718fa9f7e2bf9fe8ab02e5a87ea5f07c276b688
Alex Kellner <alexander.kellner@in2code.de>
2014-08-08 22:26:52 +0200

    Version Update

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 		of typoscript (e.g. HMENU) as a contentelement - a simple thing...',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
