c4cf28775f8f90aa1d3b432d7fbdf1b9dd678834
Patrick Lenk <Patta@users.noreply.github.com>
2019-02-27 14:31:08 +0100

    [!!!][TASK] Remove dependency to ext:cms
    PHP Deprecated:  Extension "typoscript2ce" defines a dependency on ext:cms, which has been removed. Please remove the dependency. 
    see in ...\web\typo3\sysext\core\Classes\Package\Package.php on line 133
    
    This patch removes the dependency to ext:cms, which has been removed in TYPO3 7.4.

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,10 +37,9 @@ $EM_CONF[$_EXTKEY] = [
 	'CGLcompliance_note' => '',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '6.0.0-9.5.99',
-			'extbase' => '6.0.0-9.5.99',
-			'fluid' => '6.0.0-9.5.99',
-			'cms' => '',
+			'typo3' => '7.6.0-9.5.99',
+			'extbase' => '7.6.0-9.5.99',
+			'fluid' => '7.6.0-9.5.99',
 			'php' => '5.5.0-0.0.0'
 		],
 		'conflicts' => [],
