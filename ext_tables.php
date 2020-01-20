3c3ee2070b39fc37567a76cbc9dc30144b120d4e
nebrot <nebrot@users.noreply.github.com>
2020-01-20 16:05:41 +0100

    [TASK] Replace $_EXTKEY and $TCA
    See
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87193-DeprecatedFunctionalityRemoved.html

--- ext_tables.php
+++ ext_tables.php
@@ -6,14 +6,14 @@ if (!defined('TYPO3_MODE')) {
 /**
  * Include Plugins
  */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'Pi1', 'Typoscript2ce');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('typoscript2ce', 'Pi1', 'Typoscript2ce');
 
 /**
  * Include Flexform
  */
-$pluginSignature = str_replace('_', '', $_EXTKEY) . '_pi1';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+$pluginSignature = str_replace('_', '', 'typoscript2ce') . '_pi1';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $pluginSignature,
-    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/FlexFormPi1.xml'
+    'FILE:EXT:' . 'typoscript2ce' . '/Configuration/FlexForms/FlexFormPi1.xml'
 );
