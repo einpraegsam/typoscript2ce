0b3ad2b0edc4391327b938f6da293456225ba76d
Alex Kellner <alexander.kellner@in2code.de>
2021-01-26 17:48:10 +0100

    [TASK] Remove unused configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,44 +1,18 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "typoscript2ce".
-#
-# Auto generated 02-10-2011 04:40
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'typoscript2contentelement',
 	'description' => 'typoscript2contentelement allows you to show the result
 		of typoscript (e.g. HMENU) as a contentelement - a simple thing...',
 	'category' => 'plugin',
-	'shy' => 0,
 	'version' => '3.0.1',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '',
-	'PHP_version' => '',
-	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Alex Kellner',
 	'author_email' => 'alexander.kellner@in2code.de',
 	'author_company' => 'in2code.de',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '10.0.0-10.4.99',
-			'php' => '7.2.0-0.0.0'
+			'typo3' => '10.0.0-10.4.99'
 		],
 		'conflicts' => [],
 		'suggests' => [],
